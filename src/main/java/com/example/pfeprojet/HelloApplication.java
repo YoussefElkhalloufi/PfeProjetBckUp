b40693b4e9d41b746b651fbb4a2b13329a641e5f
YoussefElkhalloufi <youssefelkhalloufi2@gmail.com>
2024-04-24 02:26:22 +0100

    ...

--- src/main/java/com/example/pfeprojet/HelloApplication.java
+++ src/main/java/com/example/pfeprojet/HelloApplication.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.util.Objects;
 
 
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
